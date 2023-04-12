640d9502b191d2f817bb274042b192d7d43aa93d
ARROUS thomas <thomas.arrous@etu.u-paris.fr>
2023-04-12 10:42:43 +0200

    Modification parsseur, ajout coordonnées. Ajout des coordonnées du première étage.

--- src/main/java/model/Noeud.java
+++ src/main/java/model/Noeud.java
@@ -27,10 +27,9 @@ public abstract class Noeud {
      */
     protected HashMap<Noeud, Double> voisins;
 
-
     // Constructeur
 
-    protected Noeud(Batiment batiment){
+    protected Noeud(Batiment batiment) {
         this.id = nbNoeud;
         nbNoeud++;
 
@@ -53,9 +52,10 @@ public abstract class Noeud {
 
     /**
      * Change l'id du noeud par {@code id}
+     * 
      * @param id
      */
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -69,78 +69,79 @@ public abstract class Noeud {
     }
 
     /**
-     * Renvoie le noeud de la map correspondant à l'id {@code id}, null s'il n'y en a pas
+     * Renvoie le noeud de la map correspondant à l'id {@code id}, null s'il n'y en
+     * a pas
+     * 
      * @param id
      * @return
      */
-    public Noeud getVoisin(int id){
-        //on parcourt la map
-        for(Map.Entry mapentry : voisins.entrySet()){
+    public Noeud getVoisin(int id) {
+        // on parcourt la map
+        for (Map.Entry mapentry : voisins.entrySet()) {
 
-            Noeud current = (Noeud)(mapentry.getKey());
+            Noeud current = (Noeud) (mapentry.getKey());
 
-            if(current.id == id){
+            if (current.id == id) {
                 return current;
             }
         }
         return null;
     }
 
-    
     // Méthodes
 
     @Override
-    public boolean equals(Object o){
-        if(!(o instanceof Noeud)){
+    public boolean equals(Object o) {
+        if (!(o instanceof Noeud)) {
             return false;
         }
 
-        Noeud comp = (Noeud)o;
+        Noeud comp = (Noeud) o;
 
-        //on compare le numéro du noeud dans le batiment
-        if(id != comp.id){
+        // on compare le numéro du noeud dans le batiment
+        if (id != comp.id) {
             return false;
         }
 
-        //on compare les voisins
+        // on compare les voisins
         return compareVoisins(comp);
     }
 
     /**
-     * Compare si les voisins de n sont les mêmes que ceux du noeud courant 
+     * Compare si les voisins de n sont les mêmes que ceux du noeud courant
+     * 
      * @param n
      * @return
      */
-    public boolean compareVoisins(Noeud n){
-        //si les noeuds n'ont pas le même nombre de voisins
-        if(voisins.size() != n.voisins.size()){
+    public boolean compareVoisins(Noeud n) {
+        // si les noeuds n'ont pas le même nombre de voisins
+        if (voisins.size() != n.voisins.size()) {
             return false;
         }
 
-        //on parcourt la map de voisins 
-        for(Map.Entry mapentry : voisins.entrySet()){
+        // on parcourt la map de voisins
+        for (Map.Entry mapentry : voisins.entrySet()) {
 
-            Noeud noeudCourant = (Noeud)mapentry.getKey();
-            Double distCourante = (Double)mapentry.getValue(); 
-            //pour chaque voisin, on regarde s'il y est dans la map de n 
+            Noeud noeudCourant = (Noeud) mapentry.getKey();
+            Double distCourante = (Double) mapentry.getValue();
+            // pour chaque voisin, on regarde s'il y est dans la map de n
 
             boolean there = false;
-            for(Map.Entry mapentryBis : n.voisins.entrySet()){
-                Noeud noeudComp = (Noeud)mapentryBis.getKey();
-                Double distComp = (Double)mapentryBis.getValue();
+            for (Map.Entry mapentryBis : n.voisins.entrySet()) {
+                Noeud noeudComp = (Noeud) mapentryBis.getKey();
+                Double distComp = (Double) mapentryBis.getValue();
 
-                if(noeudCourant.id == noeudComp.id && distCourante == distComp){
+                if (noeudCourant.id == noeudComp.id && distCourante == distComp) {
                     there = true;
                 }
             }
-            if(!there){ //on a pas trouvé d'équivalent
-                return false; 
+            if (!there) { // on a pas trouvé d'équivalent
+                return false;
             }
         }
 
         return true;
     }
-
 
     /**
      * Ajoute un voisin au noeud
